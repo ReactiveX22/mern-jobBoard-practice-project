ad0d8d875c0b3f5c88d7a79bec4f4820d9fdca06
ReactiveX22 <118966975+ReactiveX22@users.noreply.github.com>
2023-11-02 20:15:55 +0600

    feat: add JobModel, UserModel

---
+++ models/UserModel.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+// import { JOB_STATUS, JOB_TYPE } from '../utils/constants.js';
+
+const UserSchema = new mongoose.Schema(
+  {
+    name: String,
+    email: String,
+    password: String,
+
+    lastName: {
+      type: String,
+      default: 'lastname',
+    },
+    location: {
+      type: String,
+      default: 'my city',
+    },
+    role: {
+      type: String,
+      enum: ['user', 'admin'],
+      default: 'user ',
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model('User', UserSchema);
